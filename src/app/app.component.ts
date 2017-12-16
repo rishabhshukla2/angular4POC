69ed4e8a14e75263f824c7c7b5015da1d3ca71ad
unknown <rishabh.shukla2@outlook.com>
2017-12-16 01:04:04 -1200

    second commit]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   visible = true;
+  counter = 0;
   toggleDiv(){
     this.visible = !this.visible;
+    this.counter++;
   }
+  myFavLang = {
+    'frontend': ['html','css','javascript'],
+    'backend':['nodejs','php','python','ruby']
+  };
+  myFavLangArray = [
+    {'name':'html','type':'frontend'},
+    {'name':'css','type':'frontend'},
+    {'name':'js','type':'frontend'},
+    {'name':'ruby','type':'backend'},
+  ];
 }
